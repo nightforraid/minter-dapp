81e5dcb6413449f0509b0cd66aa02495c6562fac
nightforraid <nightforraid@gmail.com>
2022-05-17 14:40:22 +0700

    commit

--- backend/src/config.js
+++ backend/src/config.js
@@ -67,7 +67,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-05-18T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-05-17T7:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x93E3B867c778fb9e36577720A21a64d149f9Acc6"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
